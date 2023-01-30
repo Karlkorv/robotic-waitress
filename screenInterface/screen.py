e97736f0ed87b93b78ae930365649cd7ca388283
Felix Sjögren <felixsjogren@n153-p66.eduroam.kth.se>
2023-01-30 12:25:55 +0100

    Add class conv

---
+++ screenInterface/screen.py
@@ -0,0 +1,8 @@
+class conv :
+    def __init__(self, ID, Text, AwnsText, AwnsID):
+        self.ID = ID
+        self.Text = Text
+        self.AwnsText = AwnsText
+        self.AwnsID = AwnsID
+
+        
