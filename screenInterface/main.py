4b57e4796d96e7c2cc54b72e74d0fda40348488d
Jonatan Stagge <jstagge@kth.se>
2023-02-02 16:46:46 +0100

    Add input reader for conversation trees on the screen

--- screenInterface/main.py
+++ screenInterface/main.py
@@ -1,14 +1,63 @@
-import convNode
+import classes
 import inputReader
+import random
 
 
 def printNodes():
     for key in nodes.keys():
-        convNode = nodes.get(key)
-        print("ID: " + convNode.ID + ", Text: " + convNode.Text +
-              ", AnswText: " + convNode.AnswText + ", AnswID: " + convNode.AnswID)
+        c = nodes.get(key)
+        print("ID: " + c.ID + ", Text: " + c.Text +
+              ", AnswText: " + c.AnswText + ", AnswID: " + c.AnswID)
 
 
-nodes = inputReader.readInput('input.txt')
+def printOptions():
+    for key in options.keys():
+        o = options.get(key)
+        print("ID: " + o.ID + ", Text: " + o.Text +
+              ", ConvID: " + o.ConvID)
 
-printNodes()
+
+def printInput():
+    print("-----Options")
+    printOptions()
+    print("-----Nodes")
+    printNodes()
+
+
+def getRandomintroNode():
+    intros = []
+    for key in nodes.keys():
+        if int(key) < 1000 and int(key) > 100:
+            intros.append(nodes.get(key))
+    r = random.randrange(0, len(intros))
+    return intros[r]
+
+
+def introPhrase():
+    print(getRandomintroNode().Text)
+
+
+def displayOptions():
+    for key in range(1, len(options) + 1):
+        o = options.get(str(key))
+        print(o.ID + ": " + o.Text),
+    print("")
+
+
+options = inputReader.readInputOptions('input.txt')
+nodes = inputReader.readInputNodes('input.txt')
+
+# printInput()
+
+numOfOptions = len(options)
+selected = False
+
+introPhrase()
+displayOptions()
+while not selected:
+    selection = input()
+    if selection < 1 or selection > numOfOptions:
+        print("Invalid Selection")
+    else:
+        nextNode = (options.get(str(selection))).ConvID
+        print(nextNode)
