d5be6d405fcd0591dbb7632737ed0dc95aa79744
Jonatan Stagge <jstagge@kth.se>
2023-02-02 17:18:00 +0100

    Move functions to file, turn key in dict from string to int

--- screenInterface/main.py
+++ screenInterface/main.py
@@ -1,63 +1,23 @@
 import classes
 import inputReader
-import random
+import functions
 
-
-def printNodes():
-    for key in nodes.keys():
-        c = nodes.get(key)
-        print("ID: " + c.ID + ", Text: " + c.Text +
-              ", AnswText: " + c.AnswText + ", AnswID: " + c.AnswID)
-
-
-def printOptions():
-    for key in options.keys():
-        o = options.get(key)
-        print("ID: " + o.ID + ", Text: " + o.Text +
-              ", ConvID: " + o.ConvID)
-
-
-def printInput():
-    print("-----Options")
-    printOptions()
-    print("-----Nodes")
-    printNodes()
-
-
-def getRandomintroNode():
-    intros = []
-    for key in nodes.keys():
-        if int(key) < 1000 and int(key) > 100:
-            intros.append(nodes.get(key))
-    r = random.randrange(0, len(intros))
-    return intros[r]
-
-
-def introPhrase():
-    print(getRandomintroNode().Text)
-
-
-def displayOptions():
-    for key in range(1, len(options) + 1):
-        o = options.get(str(key))
-        print(o.ID + ": " + o.Text),
-    print("")
-
-
+intros = inputReader.readInputIntros('input.txt')
 options = inputReader.readInputOptions('input.txt')
 nodes = inputReader.readInputNodes('input.txt')
+
 
 # printInput()
 
 numOfOptions = len(options)
 selected = False
 
-introPhrase()
-displayOptions()
+functions.introPhrase(intros)
+functions.displayOptions(options, numOfOptions)
 while not selected:
     selection = input()
     if selection < 1 or selection > numOfOptions:
         print("Invalid Selection")
     else:
-        nextNode = (options.get(str(selection))).ConvID
+        nextNode = (options.get(selection)).ConvID
         print(nextNode)
