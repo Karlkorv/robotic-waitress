631bfa6d07f6b5995e0d4a5418a45155e09a1b89
erikb4 <erikb4@kth.se>
2023-02-23 14:01:27 +0100

    Add method for reading current distance value
    Co-authored-by: AdamFR00 <AdamFR00@users.noreply.github.com>

---
+++ src/rw_distance_sensors/rw_distance_sensors/Sonar.py
@@ -0,0 +1,19 @@
+import serial
+
+class Sonar():
+    
+    def __init__(self, portname, baudrate, ser):
+        self.portname = portname
+        self.baudrate = baudrate
+        self.ser = serial.Serial(self.portname, self.baudrate, timeout = 1) 
+    
+
+
+    def getDistance(self) -> float:
+
+        line = self.ser.readline()
+        if line:
+            string = line.decode()
+            value = string.strip()
+        self.ser.close()
+        return float(value)
