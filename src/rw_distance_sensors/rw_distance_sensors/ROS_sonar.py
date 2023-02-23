3a30fb7a980b5b349e7fa5619940ade03f9010ee
erikb4 <erikb4@kth.se>
2023-02-23 14:51:32 +0100

    change setup and add main function to py

---
+++ src/rw_distance_sensors/rw_distance_sensors/ROS_sonar.py
@@ -0,0 +1,46 @@
+# Authors: 
+# Erik Berglund
+# Adam Fridén Rasmussen
+# Date: 2023-02-23
+# Based on https://automaticaddison.com/create-a-basic-publisher-and-subscriber-python-ros2-foxy/ as well as 
+# https://docs.ros.org/en/foxy/Tutorials/Beginner-Client-Libraries/Writing-A-Simple-Py-Publisher-And-Subscriber.html
+
+
+import rclpy
+import rw_distance_sensors.Sonar as Sonar    
+from rw_interfaces import Ultrasonic
+from rclpy.node import Node
+
+
+class Sonar_Publisher(Node):
+    def __init__(self):
+        super().__init__('Sonar_Publisher')
+        #Create publisher that publishes to the topic 'Sonar value' with a queue limit of 10
+        self.publisher_ = self.create_publisher(Ultrasonic, 'Sonar_Value', 10)
+        # Publish message every 0.5 seconds 
+        timer_period = 0.5 
+        # A timer that calls the function timer_callback in periods of time_period. 
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        # lägg in data från sonar
+        sonar = Sonar('COM3', 9600)
+        sonarvalue = Ultrasonic()
+        sonarvalue.data = sonar.getDistance()
+        self.publisher_.publish(sonarvalue) # publicera datan från sonar till topic 'Sonar value' 
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    
+    sonar_publisher = Sonar_Publisher()
+
+    rclpy.spin(sonar_publisher)
+
+    sonar_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+
+if __name__ == '__main__':
+    main()
