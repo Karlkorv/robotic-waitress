a5cfcae9c8b80ec1020b56c5dee3d7308c682a11
Gustav Åkerström <gustavakerstrom@gmail.com>
2023-02-23 14:53:27 +0100

    Create msg for ultrasonic sensors

---
+++ install/rw_interfaces/include/rw_interfaces/rw_interfaces/msg/detail/ultrasonic__struct.h
@@ -0,0 +1,45 @@
+// generated from rosidl_generator_c/resource/idl__struct.h.em
+// with input from rw_interfaces:msg/Ultrasonic.idl
+// generated code does not contain a copyright notice
+
+#ifndef RW_INTERFACES__MSG__DETAIL__ULTRASONIC__STRUCT_H_
+#define RW_INTERFACES__MSG__DETAIL__ULTRASONIC__STRUCT_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+// Constants defined in the message
+
+// Include directives for member types
+// Member 'header'
+#include "std_msgs/msg/detail/header__struct.h"
+
+/// Struct defined in msg/Ultrasonic in the package rw_interfaces.
+typedef struct rw_interfaces__msg__Ultrasonic
+{
+  float distance;
+  std_msgs__msg__Header header;
+} rw_interfaces__msg__Ultrasonic;
+
+// Struct for a sequence of rw_interfaces__msg__Ultrasonic.
+typedef struct rw_interfaces__msg__Ultrasonic__Sequence
+{
+  rw_interfaces__msg__Ultrasonic * data;
+  /// The number of valid items in data
+  size_t size;
+  /// The number of allocated items in data
+  size_t capacity;
+} rw_interfaces__msg__Ultrasonic__Sequence;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // RW_INTERFACES__MSG__DETAIL__ULTRASONIC__STRUCT_H_
