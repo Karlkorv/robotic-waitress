083cba97dde39fb4264bce567a853f23a126a603
erikb4 <erikb4@kth.se>
2023-02-23 14:51:32 +0100

    change setup and add main function to py

--- src/rw_distance_sensors/setup.py
+++ src/rw_distance_sensors/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'hello_world = rw_distance_sensors.hello_world:main'
+            'sonar = rw_distance_sensors.ROS_sonar:main'
         ],
     },
 )
