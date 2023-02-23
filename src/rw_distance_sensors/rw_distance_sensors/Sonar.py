6c1030f5076f53194c42203ea2798254a802b4a1
erikb4 <erikb4@kth.se>
2023-02-23 14:41:12 +0100

    Change header for Distance function

--- src/rw_distance_sensors/rw_distance_sensors/Sonar.py
+++ src/rw_distance_sensors/rw_distance_sensors/Sonar.py
@@ -2,15 +2,14 @@ import serial
 
 class Sonar():
     
-    def __init__(self, portname, baudrate, ser):
+    def __init__(self, portname, baudrate):
         self.portname = portname
         self.baudrate = baudrate
         self.ser = serial.Serial(self.portname, self.baudrate, timeout = 1) 
     
 
 
-    def getDistance(self) -> float:
-
+    def getDistance(self):
         line = self.ser.readline()
         if line:
             string = line.decode()
