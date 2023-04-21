3eed4eccf5ef34f7581c85396225676d3dec6285
erikb4 <erikb4@kth.se>
2023-04-21 11:23:34 +0200

    test new way to read sensor data

--- src/rw_distance_sensors/rw_distance_sensors/Sonar.py
+++ src/rw_distance_sensors/rw_distance_sensors/Sonar.py
@@ -1,19 +1,22 @@
 import serial
+import struct
+
 class Sonar():
     
     def __init__(self, portname, baudrate):
         self.portname = portname
         self.baudrate = baudrate
-        self.ser = serial.Serial(self.portname, self.baudrate, timeout = 1) 
+        # Timeout is for how long the Pi will wait for data before returning when reading
+        self.ser = serial.Serial(self.portname, self.baudrate, timeout = 1)
     
 
 
     def getDistance(self):
-        value = None
-        line = self.ser.readline()
-        if line:
-            value = line.decode()
-        return value
+        if self.ser.in_waiting > 0:
+            data = self.ser.read(7)
+            leftSonar, centerSonar, rightSonar = struct.unpack('>iii', data)
+        self.ser.close()
+        return (leftSonar, centerSonar, rightSonar)
 
     def closePort(self):
         self.ser.close()
