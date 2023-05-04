b898e12327c1c22de59a51b930ef24e3afb5131b
Gustav Åkerström <gustavakerstrom@gmail.com>
2023-05-04 16:27:24 +0200

    Remove old logs and install folder

--- install/rw_interfaces/share/rw_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(rw_interfaces_IDL_FILES "msg/Ultrasonic.idl;msg/DetectionsVector.idl")
-set(rw_interfaces_INTERFACE_FILES "msg/Ultrasonic.msg;msg/DetectionsVector.msg")
